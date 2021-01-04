53f2438dab8bb856b641f1e2e23a3f3b010492ad
maiko-ambitage <ambitage@outlook.jp>
2021-01-04 15:33:48 +0900

    レビュー後訂正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
-    password              { password }
+    password              { "pass123" }
     password_confirmation { password }
     last_name             { '山田' }
     first_name            { '太郎' }
@@ -12,3 +11,4 @@ FactoryBot.define do
     birthday              { '2020-04-12' }
   end
 end
+    
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,6 +57,18 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
+    it 'passwordが英字のみでは登録できない' do
+      @user.password = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include 'Password is invalid'
+    end
+    it 'passeordが数字のみでは登録できない' do
+      @user.password = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include 'Password is invalid'
+    end
+
+
 
     it 'last_nameが空では登録できない' do
       @user.last_name = ''
