68ac84f679936caef649f6bfc896b123674eea5f
maiko-ambitage <ambitage@outlook.jp>
2021-01-07 16:25:32 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,12 +67,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
-      it 'priceの範囲が¥300以下では登録出来ない' do
-        @item.price = 200
+      it 'priceの範囲が¥300未満では登録出来ない' do
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
-      it 'priceの範囲が¥9,999,999以上では登録出来ない' do
+      it 'priceの範囲が¥10,000,000以上では登録出来ない' do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
