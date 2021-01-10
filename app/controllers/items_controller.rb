3ce874f6513ec4597e251211e93aa66b1dfd4cd9
maiko-ambitage <ambitage@outlook.jp>
2021-01-10 19:03:38 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   def index
     @items = Item.all
   end
@@ -21,9 +21,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
 
+    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+  end
 
-  
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      @item.update(item_params)
+      render :edit
+    end
+  end
 
   private
 
