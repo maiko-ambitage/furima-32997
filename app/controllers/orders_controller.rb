78e4a289e0114a4c082a21e72177d28b583aedfc
maiko-ambitage <ambitage@outlook.jp>
2021-01-15 21:30:26 +0900

    修正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,7 +43,9 @@ class OrdersController < ApplicationController
   end
 
   def divide_user
-    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
-    redirect_to root_path unless @item.order.nil?
+   if user_signed_in? && current_user.id == @item.user_id || @item.order.present?
+    redirect_to root_path 
+   end
   end
+   
 end
